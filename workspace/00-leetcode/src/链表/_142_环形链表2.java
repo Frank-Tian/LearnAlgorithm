3a0968e81d30c159c8be0cfd71b8c4610f4b8185
zhenzi <tianzhenzi@inke.cn>
2020-11-12 00:44:41 +0800

    Add leetcode java

---
+++ workspace/00-leetcode/src/链表/_142_环形链表2.java
@@ -0,0 +1,8 @@
+package 链表;
+
+/*
+ * https://leetcode-cn.com/problems/linked-list-cycle-ii/
+ */
+public class _142_环形链表2 {
+
+}
---
+++ workspace/00-leetcode/src/链表/_206_反转链表.java
@@ -0,0 +1,10 @@
+package 链表;
+
+/*
+ * https://leetcode-cn.com/problems/reverse-linked-list/
+ */
+public class _206_反转链表 {
+
+	// 递归实现
+	// 头插法
+}
